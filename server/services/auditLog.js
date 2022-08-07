c4715ceaa37efe8578fcab45ae880687dcf2a153
WalkingPizza <kevin.iatauro@gmail.com>
2022-08-07 18:45:40 +0200

    Context mapping function

--- server/services/auditLog.js
+++ server/services/auditLog.js
@@ -1,24 +1,29 @@
 'use strict';
 
+const pluginId = require('../utils/pluginId');
+
+const mapContext = (ctx) => {
+  return {
+    adminUserIPAddress: ctx.ips.length > 0 ? ctx.ips : ctx.ip,
+    adminUser: ctx.state.user.id,
+    url: ctx.request.url,
+    action: ctx.state.route.handler,
+    entityModel: ctx.params.model || null,
+    entityID: ctx.params.id || null,
+    plugin: ctx.state.route.info.pluginName || null,
+    responseCode: ctx.response.status,
+    responseMessage: ctx.response.message,
+  };
+};
+
 module.exports = ({ strapi }) => ({
-  async create(ctx) {
-    const auditStructure = {
-      adminUserIPAddress: ((ctx.ips.length > 0) ? ctx.ips : ctx.ip),
-      adminUser: ctx.state.user.id,
-      url: ctx.request.url,
-      action: ctx.state.route.handler,
-      entityModel: ((ctx.params.model) ? ctx.params.model : null),
-      entityID: ((ctx.params.id) ? ctx.params.id : null),
-      plugin: ((ctx.state.route.info.pluginName) ? ctx.state.route.info.pluginName : null),
-      responseCode: ctx.response.status,
-      responseMessage: ctx.response.message
-    };
+  async create(ctx, map = mapContext) {
+    const audit = map(ctx);
 
     try {
-      console.log(auditStructure)
-      return strapi.entityService.create('plugin::strapi-plugin-audit.audit-log', {
-        data: auditStructure
-      })
+      return await strapi.entityService.create(`plugin::${pluginId}.audit-log`, {
+        data: audit,
+      });
     } catch (error) {
       throw new Error(error);
     }
