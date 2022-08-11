d0b141f8ef448f20b6b02d95def38f55153f2848
WalkingPizza <kevin.iatauro@gmail.com>
2022-08-11 17:16:06 +0200

    Handle create and bulkDelete actions

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,8 +1,30 @@
 'use strict';
 
 const initAuditMiddleware = require('./middlewares/audit');
+const pluginId = require('./utils/pluginId');
+const _ = require('lodash');
 
 module.exports = ({ strapi }) => {
+  const actionsService = strapi.plugin(pluginId).service('actions');
+
+  actionsService.register('collection-types.create', 'Created an entry', ctx => {
+    const affectedEntity = _.get(ctx, 'response.body.id');
+
+    return {
+      ...actionsService.defaultHandler(ctx),
+      affectedEntities: JSON.stringify(affectedEntity ? [affectedEntity] : []),
+    };
+  });
+
+  actionsService.register('collection-types.bulkDelete', 'Deleted entries', ctx => {
+    const affectedEntities = _.get(ctx, 'request.body.ids', []);
+
+    return {
+      ...actionsService.defaultHandler(ctx),
+      affectedEntities: JSON.stringify(affectedEntities),
+    };
+  });
+
   // Init the audit middleware
   initAuditMiddleware({ strapi });
 };
